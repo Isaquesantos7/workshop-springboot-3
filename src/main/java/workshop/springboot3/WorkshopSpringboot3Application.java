85b5d55a67f9c8538c7e0027521415cef4b8e80f
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2023-01-25 19:30:16 -0300

    project structed created

---
+++ src/main/java/workshop/springboot3/WorkshopSpringboot3Application.java
@@ -0,0 +1,13 @@
+package workshop.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/workshop/springboot3/WorkshopSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package workshop.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
